09047f0a9469150d3a0c0426b7dab3fb3af59faf
Gary H <17553415+gary-lg@users.noreply.github.com>
2015-11-13 14:17:32 +0000

    Updated package references, removed .net 3.5 support

--- BuildAndPackage.bat
+++ BuildAndPackage.bat
@@ -1,3 +1,2 @@
-msbuild LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build /p:Configuration="Release-Net35"
-msbuild LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build /p:Configuration="Release-Net40"
-msbuild LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build;Package /p:Configuration="Release-Net45"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build /p:Configuration="Release-Net40"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build;Package /p:Configuration="Release-Net45"
