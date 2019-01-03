d362e16ed1f402f9bd748c59a4912c68515a2415
Gary H <17553415+gary-lg@users.noreply.github.com>
2019-01-03 19:33:12 +0000

    Update package and build to ease NuGet creation

--- BuildAndPackage.bat
+++ BuildAndPackage.bat
@@ -1,4 +1,9 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build /p:referencePath=C:\Sandbox\LeapingGorilla.Testing\packages  /p:configuration="Release-Net45"
+rd /q /s packages
+rd /q /s LeapingGorilla.Testing\bin\Release
+nuget restore LeapingGorilla.Testing.sln
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build /p:referencePath=C:\Sandbox\LeapingGorilla.Testing\packages  /p:configuration="Release"
+rd /q /s packages
+nuget restore LeapingGorilla.Testing.NetCore.sln
 dotnet build .\LeapingGorilla.Testing.NetCore.sln -c Release
 nuget pack LeapingGorilla.Testing\LeapingGorilla.Testing.nuspec
  
