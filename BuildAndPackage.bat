4498e9fd9cfb92e4574361c2a58ad260819a1349
Gary H <17553415+gary-lg@users.noreply.github.com>
2019-01-03 18:55:52 +0000

    Add change for cross-lib Emit

--- BuildAndPackage.bat
+++ BuildAndPackage.bat
@@ -1,4 +1,4 @@
-REM C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build /p:Configuration="Release-Net40"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build /p:Configuration="Release-Net45"
-REM nuget pack LeapingGorilla.Testing\LeapingGorilla.Testing.nuspec
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build /p:referencePath=C:\Sandbox\LeapingGorilla.Testing\packages  /p:configuration="Release-Net45"
+dotnet build .\LeapingGorilla.Testing.NetCore.sln -c Release
+nuget pack LeapingGorilla.Testing\LeapingGorilla.Testing.nuspec
  
