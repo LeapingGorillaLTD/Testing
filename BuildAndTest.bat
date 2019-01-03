335e44aa4677fe63bc238712f2bc3a7171f877c4
Gary H <17553415+gary-lg@users.noreply.github.com>
2019-01-03 19:44:59 +0000

    Add testing into the build batch files

--- BuildAndTest.bat
+++ BuildAndTest.bat
@@ -1,14 +1,11 @@
 rd /q /s packages
 rd /q /s LeapingGorilla.Testing\bin\Release
 nuget restore LeapingGorilla.Testing.sln
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.Testing\LeapingGorilla.Testing.csproj /t:Build /p:referencePath=C:\Sandbox\LeapingGorilla.Testing\packages  /p:configuration="Release"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.Testing.sln /t:Build /p:configuration="Release"
 
 rd /q /s packages
 nuget restore LeapingGorilla.Testing.NetCore.sln
 dotnet build .\LeapingGorilla.Testing.NetCore.sln -c Release
 
 "C:\Program Files (x86)\NUnit.org\nunit-console\nunit3-console.exe" LeapingGorilla.Testing.Tests\bin\Release\LeapingGorilla.Testing.Tests.dll
-dotnet test -c Release .\LeapingGorilla.Testing.Tests\LeapingGorilla.Testing.Tests.NetCore.csproj
-
-nuget pack LeapingGorilla.Testing\LeapingGorilla.Testing.nuspec
- 
+dotnet test .\LeapingGorilla.Testing.Tests\LeapingGorilla.Testing.Tests.NetCore.csproj
